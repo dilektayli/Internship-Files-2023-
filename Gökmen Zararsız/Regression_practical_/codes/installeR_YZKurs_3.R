f1fd8f2bb29bd109432f7d0cf4ac7e55069e3637
kdxxx <83818961+kdxxx@users.noreply.github.com>
2023-07-27 10:08:22 +0300

    Delete Gökmen Zararsız directory

--- Gökmen Zararsız/Regression_practical_/codes/installeR_YZKurs_3.R
+++
@@ -1,6 +0,0 @@
-# G.E. ZARARSIZ
-# Packages to be installed.
-pkgs.to.install <- c("ggplot2", "lattice", "caret", "doParallel", "magrittr", "pls", "grid")
-
-# Install CRAN packages
-install.packages(pkgs.to.install)
--- Gökmen Zararsız/Regression_practical_/functions/rmse.R
+++
@@ -1,7 +0,0 @@
-rmse <- function(x, y = NULL){
-  n <- length(x)
-  if (!is.null(y)){
-    x <- y - x
-  }
-  sqrt((1 / (n - 1)) * sum(x^2))
-}
