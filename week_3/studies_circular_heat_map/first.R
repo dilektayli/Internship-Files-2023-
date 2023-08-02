7eca294787e68a948fb0fcee5b44897950424a5f
kdxxx <83818961+kdxxx@users.noreply.github.com>
2023-08-02 23:59:37 +0300

    updated chapter 2 studies

--- week_3/studies_circular_heat_map/first.R
+++ week_3/studies_circular_heat_map/first.R
@@ -16,6 +16,8 @@ circos.initialize(s2, xlim = c(0, 1))
 
 # 2.8 other utilities
 # 2.8.1 -> circlize() and reverse.circlize()
+
+# data is displayed given order in the website 
 library(yaml)
 data = yaml.load_file("https://raw.githubusercontent.com/Templarian/slack-emoji-pokemon/master/pokemon.yaml")
 pokemon_list = data$emojis[1:40]
@@ -42,25 +44,9 @@ circos.track(ylim = c(0, 1), panel.fun = function(x, y) {
 }, bg.border = 1, track.height = 0.15)
 
 
+###
 
-
-# Load the required libraries
-library(yaml)
-library(circlize)
-library(EBImage)
-
-# Load Pokémon data from the provided URL
-data <- yaml.load_file("https://raw.githubusercontent.com/Templarian/slack-emoji-pokemon/master/pokemon.yaml")
-pokemon_list <- data$emojis[1:40]
-pokemon_name <- sapply(pokemon_list, function(x) x$name)
-pokemon_src <- sapply(pokemon_list, function(x) x$src)
-
-# Install and load the EBImage package
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("EBImage")
-library(EBImage)
-
+# the same pokemon data but displayed in alphabetic order
 # Set circos parameters
 circos.par("points.overflow.warning" = FALSE)
 
@@ -79,19 +65,42 @@ circos.track(ylim = c(0, 1), panel.fun = function(x, y) {
               xright = pos[1, 1] + 0.05, ytop = pos[1, 2] + 0.05)
 }, bg.border = 1, track.height = 0.15)
 
+###
+
+sectors = letters[1:10]
+circos.par(cell.padding = c(0, 0, 0, 0), track.margin = c(0, 0))
+circos.initialize(sectors, xlim =  cbind(rep(0, 10), runif(10, 0.5, 1.5)))
+circos.track(ylim = c(0, 1), track.height = mm_h(5), 
+             panel.fun = function(x, y){
+               circos.lines(c(0, 0 + mm_x(5)), c(0.5, 0.5), col = "blue")
+             })
+
+circos.track(ylim = c(0, 1), track.height = cm_h(1),
+             track.margin = c(0, mm_h(2)),
+             panel.fun = function(x, y){
+               xcenter = get.cell.meta.data("xcenter")
+               circos.lines(c(xcenter, xcenter), c(0, cm_y(1)), col = "red")
+             })
+
+circos.track(ylim = c(0, 1), track.height = inch_h((1), track.margin = c(0, mm_h(5)),
+                                                   track.margin = c(0, mm_h(5)), 
+                                                   panel.fun = function(x, y){
+                                                     line_length_on_x = cm_x(1*sqrt(2)/2)
+                                                     line_lenght_on_y = cm_y(1*sqrt(2)/2)
+                                                     circos.lines(c(0, line_lenght_on_x), c(0, line_lenght_on_y), col = "pink")
+                                                   }))
+circos.info()
 
 
+# 2.9 -> set gaps between tracks
+circos.initialize(letters[1:10], xlim = c(0, 1))
+circos.track(ylim = c(0, 1))
+set_track_gap(mm_h(2)) # 2mm
+circos.track(ylim = c(0, 1))
+set_track_gap(cm_h(0.5)) # 0.5cm
+circos.track(ylim = c(0, 1))
 
-
-
-
-
-
-
-
-
-
-
+circos.clear()
 
 
 
